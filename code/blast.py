fc6d57d853d9964afb14509714b04bb4e988e900
quant42 <yann_spoeri@web.de>
2013-11-01 13:00:36 +0100

    a ncbi test connection

--- code/blast.py
+++ code/blast.py
@@ -1,12 +1,43 @@
 #! /usr/bin/env python
 
-# This is some example code to run a blast search
+# ===================================================================
+# This class provides functions for searching for simmilar proteins
+# with the blast algorithm. Therefor blast must be installed locally
+# (Ubuntu: sudo apt-get install blast2) or the ncbi blast must be
+# available.
+# 
+# 
+# ===================================================================
+
+import socket, time
+
+class Blast():
+  
+  """ A basic blasting class """
+  
+  # a timestamp for blasting with ncbi
+  date = time()
+  
+  # constructor for building this class, if the header of the sequences, the sequences them self and the
+  # corresponding blastResults are already known
+  def __init__(self, header, sequences, blastResults):
+    pass
+  
+  @staticmethod
+  def ncbiBlast(sequence):
+    pass
+  
+  @staticmethod
+  def localBlast(header = None, seq = "NWLGVKRQPLWTLVLILWPVIIFIILAITRTKFPP", database = "../data/genes_UniProt.fasta"):
+    
+    return 
 
 def blast(seq):
   print "blast seq %s" % seq
   import commands
-  return commands.getstatusoutput("echo \"%s\" | blast2 -p blastp -d ../data/genes_UniProt.fasta" % seq)
+  blastResult = commands.getstatusoutput("echo \"%s\" | blast2 -p blastp -d ../data/genes_UniProt.fasta" % seq)
+  return Blast
 
-
+# if this is the main method, perform some basic tests
 if __name__ == "__main__":
-  print(blast("NWLGVKRQPLWTLVLILWPVIIFIILAITRTKFPP"))
+  print(blast(seq = "NWLGVKRQPLWTLVLILWPVIIFIILAITRTKFPP"))
---
+++ code/ncbi.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python
+
+# imports
+import socket, urllib, time
+
+# general settings and constants
+NCBI_BLAST_HOST = "www.ncbi.nlm.nih.gov"
+NCBI_BLAST_PATH_SEND = "/blast/Blast.cgi"
+NCBI_BLAST_PATH_SEND_PARAMS = "CMD=PUT&PROGRAM=blastp&DATABASE=nr&QUERY=%s"
+NCBI_BLAST_PATH_WAIT = "/blast/Blast.cgi?CMD=Get&FORMAT_OBJECT=SearchInfo&RID=%s"
+NCBI_BLAST_PATH_RESULTS = "/blast/Blast.cgi?CMD=Get&FORMAT_TYPE=Text&RID=%s"
+
+NCBI_BLAST_MINTIMEOUT_MS = 2000
+
+GENERAL_HTTP_POST_REQUEST  = "POST %s HTTP/1.0\r\n"
+GENERAL_HTTP_POST_REQUEST += "Host: %s\r\n"
+GENERAL_HTTP_POST_REQUEST += "Content-Length: %s\r\n"
+GENERAL_HTTP_POST_REQUEST += "Content-Type: application/x-www-form-urlencoded\r\n"
+GENERAL_HTTP_POST_REQUEST += "Connection: close\r\n"
+GENERAL_HTTP_POST_REQUEST += "\r\n"
+
+GENERAL_HTTP_GET_REQUEST  = "GET %s HTTP/1.0\r\n"
+GENERAL_HTTP_GET_REQUEST += "Host: %s\r\n"
+GENERAL_HTTP_GET_REQUEST += "Accept: text/html, */*"
+GENERAL_HTTP_GET_REQUEST += "Connection: close\r\n"
+GENERAL_HTTP_GET_REQUEST += "\r\n"
+
+# functions
+
+# this function sends an ncbi blast request
+# and returns a tuple(query Id from with which the results can be fetched later, approximate time to wait for the blasting results)
+# if nothing was given by ncbi, they are None
+def sendNcbiBlastRequest(blastRequest):
+  global NCBI_BLAST_HOST, NCBI_BLAST_PATH_SEND, NCBI_BLAST_PATH_WAIT, NCBI_BLAST_PATH_RESULTS, NCBI_BLAST_MINTIMEOUT_MS, GENERAL_HTTP_POST_REQUEST, GENERAL_HTTP_GET_REQUEST
+  # build query
+  httpRequestQuery = NCBI_BLAST_PATH_SEND_PARAMS % blastRequest
+  # build a socket, connect to ncbi and send the request
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((NCBI_BLAST_HOST, 80))
+  s.send(GENERAL_HTTP_POST_REQUEST % (NCBI_BLAST_PATH_SEND, NCBI_BLAST_HOST, len(httpRequestQuery)))
+  s.send(httpRequestQuery)
+  # ok, now get the html
+  html, rid, rtoe = "", None, None
+  while True:
+    data = s.recv(1024)
+    if len(data) == 0:
+      break
+    html += data
+  # ok parse html
+  html = html.split('\n')
+  for htmlLine in html:
+    if htmlLine.find("    RID = ") != -1:
+      rid = htmlLine[htmlLine.find("=") + 1:].strip()
+    elif htmlLine.find("    RTOE = ") != -1:
+      rtoe = htmlLine[htmlLine.find("=") + 1:].strip()
+  # in the end close the socket connection
+  s.close()
+  return (rid, rtoe)
+
+# this function check, if the requested blast search is ready, and might be fetched
+def checkIfNcbiBlastRequestIsReads(rid):
+  global NCBI_BLAST_HOST, NCBI_BLAST_PATH_SEND, NCBI_BLAST_PATH_WAIT, NCBI_BLAST_PATH_RESULTS, NCBI_BLAST_MINTIMEOUT_MS, GENERAL_HTTP_POST_REQUEST, GENERAL_HTTP_GET_REQUEST
+  # build a socket, connect to ncbi and send the request
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((NCBI_BLAST_HOST, 80))
+  s.send(GENERAL_HTTP_GET_REQUEST % (NCBI_BLAST_PATH_WAIT % rid, NCBI_BLAST_HOST))
+  # ok, now get the html
+  html, result = "", False
+  while True:
+    data = s.recv(1024)
+    if len(data) == 0:
+      break
+    html += data
+  # ok parse html
+  html = html.split('\n')
+  for htmlLine in html:
+    if htmlLine.find("Status=WAITING") != -1:
+      break                                    # not ready yet
+    elif htmlLine.find("Status=READY") != -1:
+      result = True                            # cool
+      break
+  # in the end close the socket connection
+  s.close()
+  return result
+
+def getNcbiBlastResultString(rid):
+  global NCBI_BLAST_HOST, NCBI_BLAST_PATH_SEND, NCBI_BLAST_PATH_WAIT, NCBI_BLAST_PATH_RESULTS, NCBI_BLAST_MINTIMEOUT_MS, GENERAL_HTTP_POST_REQUEST, GENERAL_HTTP_GET_REQUEST
+  # build a socket, connect to ncbi and send the request
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.connect((NCBI_BLAST_HOST, 80))
+  s.send(GENERAL_HTTP_GET_REQUEST % (NCBI_BLAST_PATH_RESULTS % rid, NCBI_BLAST_HOST))
+  # ok, now get the html
+  html = ""
+  while True:
+    data = s.recv(1024)
+    if len(data) == 0:
+      break
+    html += data
+  return html
+
+print "sending request ..."
+request = sendNcbiBlastRequest("NWLGVKRQPLWTLVLILWPVIIFIILAITRTKFPP")
+print "rid: %s rtoe: %s" % request
+ready = checkIfNcbiBlastRequestIsReads(request[0])
+print "result readyness: %s" % ready
+time.sleep(float(request[1]))
+print "check result ready ..."
+ready = False
+while not checkIfNcbiBlastRequestIsReads(request[0]):
+  time.sleep(5)
+print "result readyness: %s" % ready
+print "Fetch results ..."
+print getNcbiBlastResultString(request[0])
+
