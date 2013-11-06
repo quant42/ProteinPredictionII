c9058bc623aa51ef08cb7d6522bd068547edb698
quant42 <yann_spoeri@web.de>
2013-11-06 16:22:47 +0100

    some mixed stuff

--- code/__main__.py
+++ code/__main__.py
@@ -10,22 +10,28 @@ try:
   
   # do commandline parsing
   parser = argparse.ArgumentParser(description='Program to predict the function of proteins.')
-  parser.add_argument("-k", "--k-nearest", action="store")
-  parser.add_argument("-s", "--seq", action="store")
+  parser.add_argument("-k", "--k-nearest", action="store", dest="kNearest", default=5)
+  parser.add_argument("-s", "--seq", action="store", dest="seq")
+  parser.add_argument("-a", "--supressMessage", action="store_true", dest="supMsg")
+  parser.add_argument("-b", "--supressDebug", action="store_true", dest="supDbg")
+  parser.add_argument("-c", "--supressLog", action="store_true", dest="supLog")
+  parser.add_argument("-d", "--supressWarning", action="store_true", dest="supWarn")
+  parser.add_argument("-e", "--supressError", action="store_true", dest="supErr")
+  parser.add_argument("-f", "--supressOutput", action="store_true", dest="supOut")
   args = parser.parse_args()
+  
+  # init output
+  out.supressMessage = bool(args.supMsg)
+  out.supressDebug = bool(args.supDbg)
+  out.supressLog = bool(args.supLog)
+  out.supressWarning = bool(args.supWarn)
+  out.supressError = bool(args.supErr)
+  out.supressOutput = bool(args.supOut)
   
   # do stuff
   blastObj = blast.Blast.localBlast(seq=args.seq)
-  for dict in blastObj.hits[:int(args.k_nearest)]:
+  for dict in blastObj.hits[:int(args.kNearest)]:
     print dict["hit_id"]
-  
-#  parser.add_option("-m", "--supressMessage", dest="supMsg")
-#  parser.add_option("-d", "--supressDebug", dest="supDbg")
-#  parser.add_option("-l", "--supressLog", dest="supMsg")
-#  parser.add_option("-w", "--supressWarning", dest="supDbg")
-#  parser.add_option("-e", "--supressError", dest="supMsg")
-#  parser.add_option("-o", "--supressOutput", dest="supDbg")
-#  parser.add_option("-f", "--outputFormat", dest="supDbg")
   
   # open the fasta file with the sequences for which to predict the sequence
   
