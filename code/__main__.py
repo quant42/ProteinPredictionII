b77d5bf09da3f9e0fb56067d23c51fad3c153591
quant42 <yann_spoeri@web.de>
2013-11-07 14:07:09 +0100

    supress Msg

--- code/__main__.py
+++ code/__main__.py
@@ -12,21 +12,23 @@ try:
   parser = argparse.ArgumentParser(description='Program to predict the function of proteins.')
   parser.add_argument("-k", "--k-nearest", action="store", dest="kNearest", default=5)
   parser.add_argument("-s", "--seq", action="store", dest="seq")
-  parser.add_argument("-a", "--supressMessage", action="store_true", dest="supMsg")
-  parser.add_argument("-b", "--supressDebug", action="store_true", dest="supDbg")
-  parser.add_argument("-c", "--supressLog", action="store_true", dest="supLog")
-  parser.add_argument("-d", "--supressWarning", action="store_true", dest="supWarn")
+  parser.add_argument("-m", "--supressMessage", action="store_true", dest="supMsg")
+  parser.add_argument("-d", "--supressDebug", action="store_true", dest="supDbg")
+  parser.add_argument("-l", "--supressLog", action="store_true", dest="supLog")
+  parser.add_argument("-w", "--supressWarning", action="store_true", dest="supWarn")
   parser.add_argument("-e", "--supressError", action="store_true", dest="supErr")
-  parser.add_argument("-f", "--supressOutput", action="store_true", dest="supOut")
+  parser.add_argument("-o", "--supressOutput", action="store_true", dest="supOut")
+  parser.add_argument("-f", "--outputFormat", action="store", dest="outForm", default="bash")
   args = parser.parse_args()
   
   # init output
-  out.supressMessage = bool(args.supMsg)
-  out.supressDebug = bool(args.supDbg)
-  out.supressLog = bool(args.supLog)
-  out.supressWarning = bool(args.supWarn)
-  out.supressError = bool(args.supErr)
-  out.supressOutput = bool(args.supOut)
+  out.supressMessage = args.supMsg
+  out.supressDebug = args.supDbg
+  out.supressLog = args.supLog
+  out.supressWarning = args.supWarn
+  out.supressError = args.supErr
+  out.supressOutput = args.supOut
+  out.outputFormat = args.outForm
   
   # do stuff
   blastObj = blast.Blast.localBlast(seq=args.seq)
@@ -34,8 +36,6 @@ try:
     print dict["hit_id"]
   
   # open the fasta file with the sequences for which to predict the sequence
-  
-  
   
 except Exception as err:
   # main routine exception handler
