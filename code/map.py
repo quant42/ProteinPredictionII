860c4fab4bc441cd30dd97dbf760457ad9e5dfd4
boidol <jonathan@boidol.com>
2013-11-13 08:53:22 +0100

    reduced annotations

--- code/map.py
+++ code/map.py
@@ -3,9 +3,38 @@ from collections import defaultdict
 fasta_file = '../data/genes_UniProt.fasta'
 annotation_file = '../data/ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype.txt'
 mapping_file = '../data/automatic_mapping'
-mapping_Uniprot_Entrez = defaultdict(list) 
+hpo_file = '../data/hp.obo'
+mapping_Uniprot_Entrez = defaultdict(list)
+REDUCE = True
+
+def hpo_tree(filename):
+    tree = {}
+    node_id = ''
+    for line in open(filename):
+        if line.startswith('id:'):
+            node_id = line.split()[-1]
+            tree[node_id] = {'childnodes': [],'parentnodes': [], 'visited': 0}
+            #TODO skip obsolete nodes
+    for line in open(filename):
+        if line.startswith('id:'):
+            node_id = line.split()[-1]
+        if line.startswith('is_a:'):
+            parentnode = line.split()[1]
+            tree[node_id]['parentnodes'].append(parentnode)
+            tree[parentnode]['childnodes'].append(node_id)
+            
+    return tree
+
+def has_children(tree, node_id, set_of_children):
+    for child in tree[node_id]['childnodes']:
+        if child in set_of_children:
+            return True
+        if has_children(tree, child, set_of_children):
+            return True
+    return False
 
 uniprot = {}
+hpo_tree = hpo_tree(hpo_file)
 
 for line in open(fasta_file):
     if line.startswith('>'):
@@ -30,14 +59,22 @@ for k in uniprot:
     for eg in mapping_Uniprot_Entrez[k]:
         if eg in entrez:
             annotations = annotations.union(entrez[eg])
+    reduced_annotations = set([])
+    for node in annotations:
+        if not has_children(hpo_tree, node, annotations):
+            reduced_annotations.add(node)
     if len(annotations) == 0:
         print "%s\tno-annotation"%k
-    else:            
-        print "%s\t%s"%(k,','.join(annotations))
-        if len(annotations) > max_ann:
-            max_ann = len(annotations)
+    else:
+        len_annotations = 0
+        if REDUCE:
+            len_annotations = len(reduced_annotations)
+            #print "%s\t%s"%(k,','.join(reduced_annotations))
+        else:
+            len_annotations = len(annotations)
+            #print "%s\t%s"%(k,','.join(annotations))
+            
+        if len_annotations > max_ann:
+            max_ann = len_annotations
             prot = k
-        pass
-print max_ann, prot
-
         
