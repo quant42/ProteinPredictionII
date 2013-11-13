44af6f2de57fbd218d4b05bddb2908d3436b02fd
quant42 <yann_spoeri@web.de>
2013-11-13 19:17:28 +0100

    hpo parser

--- code/hpoParser.py
+++ code/hpoParser.py
@@ -10,9 +10,11 @@ class HpoGraph():
     
     """ initalize an hpo graph by an hpo file """
     
+    # debug message
+    out.writeDebug( "parsing hpo file " + str( hpoFile ) )
     # init main class varibale
-    self.hpoTerms = []
-    self.hpoGraph = {}
+    self.hpoTermsLst = []
+    self.hpoTermsDict = {}
     # helper function to analyse the lines
     def _analyseLines(self, lines):
       
@@ -21,21 +23,32 @@ class HpoGraph():
       # file descriptor or hp term?
       if lines[0].startswith( "[Term]" ):
         # add a hpoterm by the hpoterms description
-        self.hpoTerms.append( HpoTerm( lines ) )
+        term = HpoTerm( lines[1:] )
+        self.hpoTermsLst.append( term )
+        self.hpoTermsDict.update( { term.id : term } )
       else:
         for line in lines:
-          if line.strip() != "":
-            # ok, get the position of the :
-            attrName = line[:line.find(":")].strip()
-            attrVal = line[line.find(":")+1:].strip()
-            # now add this as attribute
-            print attrName + ": " + attrVal
+          # ok, get the position of the :
+          attrName = line[:line.find(":")].strip()
+          attrVal = line[line.find(":")+1:].strip()
+          # now add this as attribute
+          if hasattr(self, attrName):
+            if isinstance(getattr(self, attrName), list):
+              getattr(self, attrName).append( attrVal )
+            else:
+              setattr(self, attrName, [ getattr(self, attrName), attrVal ])
+          else:
+            setattr(self, attrName, attrVal)
     
     # ok, parse the lines in the file
     try:
       f = file( hpoFile, "r" )
       lines = []
-      for line in lines:
+      for line in f:
+        # skip empty lines
+        if line.strip() == "":
+          continue
+        # do something for non empty lines
         if line.startswith( "[Term]" ):
           _analyseLines(self, lines)
           lines = [ line ]
@@ -46,15 +59,40 @@ class HpoGraph():
     except Exception as e:
       out.writeError("Error parsing hpo file " + str( e.message ) + " " + str( e.args) )
   
-  def getSubGraph(self):
-    pass
+  def getHpoTermById(self, id):
+    
+    """ returns an hpo term by an id """
+    
+    try:
+      return self.hpoTermsDict[id]
+    except KeyError:
+      return None
 
 class HpoTerm():
   
   """ This is a class representing a single hpoterm """
   
-  def __init__(self, hpoTermStr):
-    pass
+  def __init__(self, hpoTermLines):
+    
+    """ This initalize a hpoterm by the lines of an [Term] in an hpo file """
+    
+    for line in hpoTermLines:
+      # ok, thats a good line with a good description
+      # parse for :
+      attrName = line[:line.find(":")].strip()
+      attrVal = line[line.find(":")+1:].strip()
+      # ok, now append this attr
+      if hasattr(self, attrName):
+        if isinstance(getattr(self, attrName), list):
+          getattr(self, attrName).append( attrVal )
+        else:
+          setattr(self, attrName, [ getattr(self, attrName), attrVal ])
+      else:
+        setattr(self, attrName, attrVal)
 
 if __name__ == "__main__":
   graph = HpoGraph()
+  print dir(graph)
+  print dir(graph.getHpoTermById("HP:0000002"))
+  print len(graph.hpoTermsDict)
+  print len(graph.hpoTermsLst)
