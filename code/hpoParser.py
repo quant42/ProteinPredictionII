2278a2bfa101e4c5eddf8ea4a299d881cca9ae86
quant42 <yann_spoeri@web.de>
2013-11-13 23:11:46 +0100

    some stuff

--- code/hpoParser.py
+++ code/hpoParser.py
@@ -13,8 +13,10 @@ class HpoGraph():
     # debug message
     out.writeDebug( "parsing hpo file " + str( hpoFile ) )
     # init main class varibale
-    self.hpoTermsLst = []
     self.hpoTermsDict = {}
+    # if the file to parse is None, an empty HpoGraph will be returned
+    if hpoFile == None:
+      return
     # helper function to analyse the lines
     def _analyseLines(self, lines):
       
@@ -24,7 +26,6 @@ class HpoGraph():
       if lines[0].startswith( "[Term]" ):
         # add a hpoterm by the hpoterms description
         term = HpoTerm( lines[1:] )
-        self.hpoTermsLst.append( term )
         self.hpoTermsDict.update( { term.id : term } )
       else:
         for line in lines:
@@ -61,12 +62,29 @@ class HpoGraph():
   
   def getHpoTermById(self, id):
     
-    """ returns an hpo term by an id """
+    """ returns an hpo term by an hpo id """
     
     try:
-      return self.hpoTermsDict[id]
+      return self.hpoTermsDict[id.split(" ")[0]]
     except KeyError:
       return None
+  
+  def getHpoSubGraph(self, idLst):
+    
+    """ build a hpo sub tree containing only the id in the idList and there parents """
+    
+    # create a new Hpo(Sub)Graph to return
+    ret = HpoGraph(None)
+    # ok, put in the stuff in
+    for id in idLst:
+      self.hpoTermsDict.update( { id : self.getHpoTermById( id ) } )
+    # return subgraph
+    return ret
+  
+  def __add__(self, other):
+    pass
+  def __sub__(self, other):
+    pass
 
 class HpoTerm():
   
@@ -93,6 +111,6 @@ class HpoTerm():
 if __name__ == "__main__":
   graph = HpoGraph()
   print dir(graph)
-  print dir(graph.getHpoTermById("HP:0000002"))
+  print dir(graph.getHpoTermById("HP:0000008"))
+  print graph.getHpoTermById("HP:0000008").is_a
   print len(graph.hpoTermsDict)
-  print len(graph.hpoTermsLst)
