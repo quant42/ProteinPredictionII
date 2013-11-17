8329bc8a27f4c5872b452aa95383e378b9e45c53
quant42 <yann_spoeri@web.de>
2013-11-17 21:08:33 +0100

    stuff

--- code/__main__.py
+++ code/__main__.py
@@ -4,40 +4,76 @@ import out
 
 try:
   # import stuff
-  #from Bio import SeqIO
-  import argparse, blast, hhblits, map, hpoParser
+  from Bio import SeqIO
+  import argparse, blast, hhblits, map, hpoParser, os
   
   # do commandline parsing
-  # "", "", action=[store, store_true], dest=varname, type=[int, str, ...], help=..., choices=[0, 1, 2], default=0, required=False/True
-  # nargs="+" (arg must be giveb), "?"(arg optional)
-  # metavar='N' extended argument
-  # group = parser.add_mutually_exclusive_group()
-  # group.add_argument("-v", "--verbose", action="store_true")
-  # group.add_argument("-q", "--quiet", action="store_true")
-  # 
-  # 
   parser = argparse.ArgumentParser(description='This program should predict the function of proteins.')
-  group = parser.add_mutally_exclusive_group()
+  group = parser.add_mutually_exclusive_group(required=True)
   group.add_argument("-s", "--seq", action="store", dest="sequence", type=str, help="The seuqence in one letter code to predict the function for!")
   group.add_argument("-f", "--file", action="store", dest="fastaFile", type=str, help="A fasta file containing the protein sequences to predict there functions!")
+  parser.add_argument("--svg", action="store_true", dest="createSvgImage", help="whether to create a svg image of the graph!")
+  parser.add_argument("-d", "--debug", dest="debugFile", type=str, default=None, required=False, help="The debug file to write the debug/log/error messages to! (default: stderr)")
+  parser.add_argument("-v", "--verbose", dest="verbosity", type=int, default=0, required=False, help="set the verbosity of streams as sum 1=Message, 2=Debug, 4=Log, 8=Warning, 16=Error, 32=Output!")
+  parser.add_argument("-w", "--outFormat", dest="outputFormat", type=str, choices=["bash", "plain", "html"], default="bash", required=False, help="set the output format to bash, plain or html!")
+  parser.add_argument("-p", "--hpoFile", dest="hpoFile", type=str, default="../data/hp.obo", required=False, help="The path to the file with the current hpo definition!")
   args = parser.parse_args()
   
-  # init output
-#  out.supressMessage = args.supMsg
-#  out.supressDebug = args.supDbg
-#  out.supressLog = args.supLog
-#  out.supressWarning = args.supWarn
-#  out.supressError = args.supErr
-#  out.supressOutput = args.supOut
-#  out.outputFormat = args.outForm
+  # init output format
+  out.supressMessage = bool(args.verbosity >> 0 & 1)
+  out.supressDebug = bool(args.verbosity >> 1 & 1)
+  out.supressLog = bool(args.verbosity >> 2 & 1)
+  out.supressWarning = bool(args.verbosity >> 3 & 1)
+  out.supressError = bool(args.verbosity >> 4 & 1)
+  out.supressOutput = bool(args.verbosity >> 5 & 1)
+  out.outputFormat = args.outputFormat
   
-  # do stuff
-  blastObj = blast.Blast.localBlast(seq=args.seq)
-  for dict in blastObj.hits[:int(args.kNearest)]:
-    print dict["hit_id"]
+  # init the hpoParser
+  hpoGraph = None
+  if os.path.isfile(args.hpoFile):
+    hpoGraph = hpoParser.HpoGraph(hpoFile=args.hpoFile)
+  else:
+    out.writeLog("missing hpoFile! Try standard hpoFile in the data directory")
+    hpoGraph = hpoParser.HpoGraph()
   
-  # open the fasta file with the sequences for which to predict the sequence
+  # prediction method
+  def predictSequence(args, hpoGraph, name="Sequence", seq=""):
+    # ok, do the whole thing
+    try:
+      # debug msg
+      out.writeDebug( "Predict function for protein: id: \"" + str( name ) +  "\" sequence: \"" + str( seq ) +"\"" )
+      
+      # ok, first of all, get similar sequences!
+      
+      # build the trees
+      
+      # do the prediciton
+      
+      # svg image desired?
+      
+      # clear attrs from all tree nodes, so that these don't interfere with later predictions
+      hpoGraph.clearAttr()
+      
+    except Exception as err:
+      out.writeError("Predicting Error: " + err)
+      exit(1)
+    pass
+  
+  # ok, do the whole thing
+  if args.sequence != None:
+    predictSequence(args, hpoGraph, seq=args.sequence)
+  elif os.path.isfile(args.fastaFile):
+    f = open(args.fastaFile, "rU")
+    for record in SeqIO.parse(f, "fasta"):
+      predictSequence(args, hpoGraph, name=record.id, seq=record.seq)
+    f.close()
+  else:
+    out.writeError("Error: no sequence to predict given! (wrong path?)")
+  
+  # quit without error code
+  exit(0)
   
 except Exception as err:
   # main routine exception handler
-  out.writeError(err)
+  out.writeError("Unexpected Error: " + err)
+  exit(1)
