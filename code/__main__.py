34fe6e3f3271490b8607968d2323d1fa58e7a771
Yann Spoeri <spoeri@i12k-biolab14.informatik.tu-muenchen.de>
2013-11-21 11:23:19 +0100

    : missing

--- code/__main__.py
+++ code/__main__.py
@@ -98,7 +98,7 @@ try:
       out.writeDebug("Run main prediction!")
       # TODO: implement a better predictor - for example with an neuronal network (pyBrain etc.)
       # accept those with more than two results
-      if graph != None
+      if graph != None:
         for node in graph.hpoTermsDict:
           if len( graph.hpoTermsDict[ node ].attributes ) > 2:
             graph.hpoTermsDict[ node ].accepted = True
