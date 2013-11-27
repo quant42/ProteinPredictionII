f216dbe5ca62689a02583689ff056198e8bae88a
Yann Spöri <yann_spoeri@web.de>
2013-11-27 18:34:26 +0100

    stuff

---
+++ code/predictor.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+# this is the predictor function, that runs the prediction for a graph
+
+# imports
+import pickle, features
+
+class Predictor():
+  
+  """ The predictor for a node """
+  
+  def __init__(self, netFile):
+    
+    """ Initalize a predictor with a querySequence and the corresponding graph """
+    
+    self.querySequence  = querySequence
+    self.graph = graph
+  
+  def runprediction(self, querySequence, graph):
+    pass
