dc0216a25c0ab64575d6f7d8136e55e9db955524
Yann Spoeri <spoeri@rostlab.informatik.tu-muenchen.de>
2013-12-09 12:45:35 +0100

    stuff

--- code/preCalc.py
+++ code/preCalc.py
@@ -3,14 +3,14 @@
 # This does a precalculation for blast and hhblits
 
 import blast, hhblits
+import sys
 
 blastDB = "../data/genes_UniProt.fasta"
 hhDB = "../data/PP2db"
-out = "../data/lookup0.dat"
-fastaFile = "../data/dataset_0.fa"
+out = "../data/lookup{}.dat".format(sys.argv[1])
+fastaFile = "../data/dataset_{}.fa".format(sys.argv[1])
 
 def predictSequence(file, id, seq, dbBlast, dbHHB, minEValBlast = 0.1):
-  print("Do record: " + str(record.id))
   g.write("{}\n".format(id))
   blastResults = blast.Blast.localBlast(seq=seq, database=dbBlast, minEVal=minEValBlast)
   for hit in blastResults.hits:
@@ -20,10 +20,12 @@ def predictSequence(file, id, seq, dbBlast, dbHHB, minEValBlast = 0.1):
     g.write("\t{}\t{}\t{}\t{}\t{}\t{}\n".format(hit["method"], hit["hit_id"], hit["hit_value"], hit["hit_from"], hit["hit_to"], hit["hit_order"]))
 
 from Bio import SeqIO
+#from os import listdir
 
 g = open(out, "w+")
 f = open(fastaFile, "rU")
 for record in SeqIO.parse(f, "fasta"):
   predictSequence(g, record.id, str(record.seq), blastDB, hhDB)
+  g.flush()
 f.close()
 g.close()
